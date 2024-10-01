513342c0af18ab62b1c8f563e6efd80b5dcb2e30
Sebastien Fenelon <fenelonsebastien@outlook.com>
2024-10-01 13:57:37 -0500

    Arithmetic lab

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a,b){
+return (a + b)
+}
+
+function subtract(a,b){
+return (a - b)
+}
+
+function multiply(a,b){
+return (a * b)
+}
+
+function divide(a,b){
+return (a / b)
+}
+
+function increment(n){
+    return ++n
+}
+
+function decrement(n){
+    return --n
+}
+
+function makeInt(n){
+   console.log(parseInt("15",10))
+}
+
+
+function makeInt(n){
+          parseInt('n',10)
+ }
+
+
+ function preserveDecimal(n){
+    return parseFloat("n")
+ }
